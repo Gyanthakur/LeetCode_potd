fadaab502f24d84914b1211cf7450706051976e1
Gyanthakur <98226958+Gyanthakur@users.noreply.github.com>
2023-05-11 19:39:48 +0530

    11_may_2k23

---
+++ may_2023/potd_11_05_2023.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+return 0;
+}
+
+class Solution {
+public:
+    int maxUncrossedLines(vector<int>& nums1, vector<int>& nums2) {
+        int m = nums1.size(), n = nums2.size();
+        if (m < n) {
+            swap(nums1, nums2);
+            swap(m, n);
+        }
+        vector<int> dp(n + 1);
+        for (int i = 1; i <= m; i++) {
+            int prev = 0;
+            for (int j = 1; j <= n; j++) {
+                int curr = dp[j];
+                if (nums1[i-1] == nums2[j-1]) {
+                    dp[j] = prev + 1;
+                } else {
+                    dp[j] = max(dp[j-1], curr);
+                }
+                prev = curr;
+            }
+        }
+        return dp[n];
+    }
+};
