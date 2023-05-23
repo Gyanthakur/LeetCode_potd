51d64d169126ac2ffb9f92a1edbd8d397da5caca
Gyanthakur <98226958+Gyanthakur@users.noreply.github.com>
2023-05-23 12:25:50 +0530

    23_may_2k23

---
+++ may_2023/potd_23_05_2023.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+return 0;
+}
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * KthLargest* obj = new KthLargest(k, nums);
+ * int param_1 = obj->add(val);
+ */
+
+class KthLargest {
+private:
+    int k;
+    std::priority_queue<int, std::vector<int>, std::greater<int>> minHeap;
+
+public:
+    KthLargest(int k, vector<int>& nums) {
+        this->k = k;
+        for (int num : nums) {
+            add(num);
+        }
+    }
+
+    int add(int val) {
+        if (minHeap.size() < k) {
+            minHeap.push(val);
+        } else if (val > minHeap.top()) {
+            minHeap.pop();
+            minHeap.push(val);
+        }
+        return minHeap.top();
+    }
+};
