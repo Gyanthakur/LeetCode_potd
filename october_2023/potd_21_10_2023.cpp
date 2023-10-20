55681fe5aaa6f18701ead760b27fd2e458fb602d
Yogesh Sharma <yogeshhh.1207@gmail.com>
2023-10-21 01:40:04 +0530

    added POTD for 21-10-2023

---
+++ october_2023/potd_21_10_2023.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+return 0;
+}
+
+class NestedIterator {
+public:
+    NestedIterator(vector<NestedInteger> &nestedList) {
+        begins.push(nestedList.begin());
+        ends.push(nestedList.end());
+    }
+
+    int next() {
+        hasNext();
+        return (begins.top()++)->getInteger();
+    }
+
+    bool hasNext() {
+        while (begins.size()) {
+            if (begins.top() == ends.top()) {
+                begins.pop();
+                ends.pop();
+            } else {
+                auto x = begins.top();
+                if (x->isInteger())
+                    return true;
+                begins.top()++;
+                begins.push(x->getList().begin());
+                ends.push(x->getList().end());
+            }
+        }
+        return false;
+    }
+
+private:
+    stack<vector<NestedInteger>::iterator> begins, ends;
+};
